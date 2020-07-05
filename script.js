e174c76657cd096fa5952389f2bfe892823231ac
Patryk <thepatrvq@gmail.com>
2020-07-05 16:35:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,96 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
+
+
+function showLoadingSpiner() {
+    loader.hidden = false
+    quoteContainer.hidden = true
+}
+
+function hideLoadingSpiner() {
+    if (!loader.hidden) cancelAnimation()
+    
+    function cancelAnimation() {
+        quoteContainer.hidden = false
+        loader.hidden = true
+    }
+}
+
+// Get Quote From API
+let requestErrorCounter = 0;
+let duplicatedQuoteRequestCounter = 0;
+async function getQuote() {
+
+
+    showLoadingSpiner()
+    try {
+        const responseData = await connectToQuoteAPI()
+
+        if (quoteTextIsDuplicated(responseData.quoteText)) {
+            showLoadingSpiner()
+            console.log('quote was duplicated')
+            duplicatedQuoteRequestCounter < 20 ? getQuote() : alert('try again later!')
+            
+        } else {
+            assingAuthorName(responseData)
+            reduceFontSizeForLongQuote(responseData.quoteText)
+            assingQuoteText(responseData)
+            hideLoadingSpiner()
+        }
+        
+    } catch (error) {
+        requestErrorCounter += 1;
+        requestErrorCounter < 20 ? getQuote() : alert('try again later!');
+        console.log('whoops, no quote', error);
+        hideLoadingSpiner()
+    }
+    
+
+    function quoteTextIsDuplicated(text) {
+        return document.getElementById('quote').textContent === text 
+    }
+
+    function assingQuoteText(responseData) {
+        quoteText.innerText = responseData.quoteText
+    }
+
+    function assingAuthorName(responseData) {
+        authorText.innerText = responseData.quoteAuthor || '-Unknow'
+    }
+
+    async function connectToQuoteAPI() {
+        const proxyUrl = 'https://evening-ravine-24129.herokuapp.com/'
+        const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    
+        const response = await fetch(proxyUrl + apiUrl)
+        const responseData = await response.json()
+        return responseData
+    }
+
+    function reduceFontSizeForLongQuote(text) {
+        text.length > 100 
+        ? quoteText.classList.add('long-quote') 
+        : quoteText.classList.remove('long-quote')
+    }
+
+    
+}
+
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote}
+    -${author}`;
+    window.open(twitterUrl, '_bloank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+getQuote()
+showLoadingSpiner()
